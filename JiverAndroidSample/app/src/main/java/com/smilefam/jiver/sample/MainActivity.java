af72e9cf39bb83eaf9c0988d5cb379ec92c05151
Harry Kim <harry.kim@smilefam.com>
2015-08-19 16:31:16 +0900

    == 1.1.21 == 1. Updated JIVER SDK to 1.1.21. 2. Minor UI fix.

--- JiverAndroidSample/app/src/main/java/com/smilefam/jiver/sample/MainActivity.java
+++ JiverAndroidSample/app/src/main/java/com/smilefam/jiver/sample/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends FragmentActivity {
     private static final int REQUEST_JIVER_MESSAGING_CHANNEL_LIST_ACTIVITY = 201;
     private static final int REQUEST_JIVER_MEMBER_LIST_ACTIVITY = 300;
 
-    public static String VERSION = "1.1.17.0";
+    public static String VERSION = "1.1.21.0";
 
     final String appId = "A7A2672C-AD11-11E4-8DAA-0A18B21C2D82"; /* Sample Jiver Application */
     String userId = JiverChatActivity.Helper.generateDeviceUUID(MainActivity.this); /* Generate Device UUID */
